ad7c1c5e94d55777f0853fde656cc5b0dc441035
Mohamed Ahmed <mohamedahmedfathy28@gmail.com>
2026-04-17 21:13:03 +0200

    chore(infrastructure): add DbContext and register DI services
    - Implemented ApplicationDbContext for domain entity persistence
    - Configured entity sets and relationships for plan, subscription, organization, and addon
    - Registered DbContext in DI container with proper lifetime
    - Added infrastructure layer services to dependency injection for clean architecture integration

--- Viora/Viora.Infrastructure/ApplicationDbContext.cs
+++ Viora/Viora.Infrastructure/ApplicationDbContext.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using Viora.Application.Abstractions.Clock;
+using Viora.Application.Abstractions.Exceptions;
 using Viora.Domain.Abstractions;
 
 
@@ -6,8 +10,57 @@ namespace Viora.Infrastructure;
 
 internal class ApplicationDbContext : DbContext, IUnitOfWork
 {
-    public override Task<int> SaveChangesAsync(CancellationToken token = default)
+    private readonly IDateTimeProvider _dateTimeProvider;
+    private readonly IPublisher _publisher;
+
+    public ApplicationDbContext(
+        DbContextOptions options,
+        IDateTimeProvider dateTimeProvider,
+        IPublisher publisher
+        ) : base(options)
     {
-        throw new NotImplementedException();
+        _publisher = publisher;
+        _dateTimeProvider = dateTimeProvider;
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public async Task<int> SaveChangeAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await PuplishDomainEventAsync(cancellationToken);
+
+            var result = await base.SaveChangesAsync(cancellationToken);
+
+
+            return result;
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            throw new ConcurrencyException("Concurrency exception occurred.", ex);
+
+        }
+    }
+
+    public async Task PuplishDomainEventAsync(CancellationToken cancellationToken)
+    {
+        var domainEntities = ChangeTracker.Entries<Entity>()
+            .Where(e => e.Entity.DomainEvents != null && e.Entity.DomainEvents.Any())
+            .Select(e => e.Entity)
+            .ToList();
+        var domainEvents = domainEntities
+            .SelectMany(e => e.DomainEvents)
+            .ToList();
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent, cancellationToken);
+        }
+        domainEntities.ForEach(e => e.ClearDomainEvents());
+    }
+
 }
---
+++ Viora/Viora.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Viora.Application.Abstractions.Clock;
+using Viora.Domain.Abstractions;
+using Viora.Domain.Organizations;
+using Viora.Domain.Plans;
+using Viora.Domain.Plans.Features;
+using Viora.Domain.Subscriptions;
+using Viora.Infrastructure.Clock;
+using Viora.Infrastructure.Repositories;
+
+namespace Viora.Infrastructure;
+
+internal class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(IServiceCollection services, IConfiguration configuration)
+    {
+
+        var ConnectionString = configuration.GetConnectionString("Database");
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(ConnectionString));
+        services.AddScoped<IPlanRepository, PlanRepository>();
+        services.AddScoped<IFeatureUsageRepository, FeatureUsageRepository>();
+        services.AddScoped<IPlanFeatureRepository, PlanFeatureRepository>();
+        services.AddScoped<IOrganizationRepository, OrganziationRepository>();
+        services.AddScoped<ILimitedFeatureRepository, LimitedFeatureRepository>();
+        services.AddScoped<IFeatureRepository, FeatureRepository>();
+        services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
+
+        services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+
+        services.AddScoped<IUnitOfWork>(provider => provider.GetRequiredService<ApplicationDbContext>());
+        return services;
+
+    }
+}
