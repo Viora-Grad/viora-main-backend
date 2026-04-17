c1ef0b2e22975595aa70c49a1a1df926e74833f1
Mohamed Ahmed <mohamedahmedfathy28@gmail.com>
2026-04-17 21:11:56 +0200

    chore(domain): add configuration for all domain classes
    - Implemented entity configurations for domain classes
    - Applied consistent naming conventions and property mappings
    - Configured relationships and constraints to enforce domain rules
    - Centralized configuration logic for maintainability and clarity

---
+++ Viora/Viora.Infrastructure/Configurations/LimitedFeatureAddonConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Viora.Domain.Plans.Addons;
+using Viora.Domain.Plans.Features;
+
+namespace Viora.Infrastructure.Configurations;
+
+internal sealed class LimitedFeatureAddonConfiguration : IEntityTypeConfiguration<LimitedFeatureAddon>
+{
+    public void Configure(EntityTypeBuilder<LimitedFeatureAddon> builder)
+    {
+        builder.ToTable("LimitedFeatureAddons");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.RestoreValue)
+            .IsRequired();
+
+        builder.Property(x => x.Price)
+            .HasPrecision(18, 2);
+
+        builder.HasOne<LimitedFeature>()
+            .WithMany()
+            .HasForeignKey(x => x.LimitedFeatureId);
+    }
+}
---
+++ Viora/Viora.Infrastructure/Configurations/SubscriptionConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Viora.Domain.Organizations;
+using Viora.Domain.Plans;
+using Viora.Domain.Subscriptions;
+
+namespace Viora.Infrastructure.Configurations;
+
+internal sealed class SubscriptionConfiguration : IEntityTypeConfiguration<Subscription>
+{
+    public void Configure(EntityTypeBuilder<Subscription> builder)
+    {
+        builder.ToTable("Subscriptions");
+
+
+        builder.HasKey(x => x.Id);
+
+
+        builder.Property(x => x.PlanId)
+            .IsRequired();
+
+        builder.HasOne<Plan>()
+            .WithMany()
+            .HasForeignKey(x => x.PlanId);
+
+        builder.Property(x => x.OrganizationId)
+            .IsRequired();
+
+        builder.HasOne<Organization>()
+            .WithMany()
+            .HasForeignKey(x => x.OrganizationId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Property(x => x.Status)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(x => x.SubscriptionsStartTime)
+            .IsRequired();
+
+        builder.Property(x => x.SubscriptionsEndTime)
+            .IsRequired();
+
+
+    }
+}
