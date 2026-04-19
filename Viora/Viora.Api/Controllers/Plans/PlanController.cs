c65a562a9de71d4c66889ded5ab39708074f8d10
Mohamed Ahmed <mohamedahmedfathy28@gmail.com>
2026-04-19 16:40:00 +0200

    feat(api): add Subscription and Plan controllers with endpoints
    - Implemented SubscriptionController with endpoints for create, change, renew, and query operations
    - Added PlanController with endpoints to retrieve all plans, single plan, and associated features
    - Integrated controllers with Mediator commands and queries for clean separation
    - Ensured proper routing and response formatting for API consumers

---
+++ Viora/Viora.Api/Controllers/Plans/PlanController.cs
@@ -0,0 +1,75 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Viora.Application.Plans.GetFeatureAddon;
+using Viora.Application.Plans.GetPlanById;
+using Viora.Application.Plans.GetPlans;
+using Viora.Application.Plans.PurchaseAddon;
+
+namespace Viora.Api.Controllers.Plans;
+
+[ApiController]
+[Route("api/plan")]
+
+public class PlanController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public PlanController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet]
+    [Route("/get/{planId}")]
+    public async Task<IActionResult> GetPlan(
+        Guid planId,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetPlanByIdQuery(planId);
+        var result = await _sender.Send(query);
+        if (result.IsSuccess)
+            return Ok(result.Value);
+        else
+            return NotFound(result.Error);
+
+    }
+
+    [HttpGet]
+    [Route("/getAll")]
+    public async Task<IActionResult> GetAllPlans(
+        CancellationToken cancellationToken)
+    {
+        var query = new GetPlansQuery();
+        var result = await _sender.Send(query);
+        if (result.IsSuccess)
+            return Ok(result.Value);
+        else
+            return NotFound(result.Error);
+    }
+
+    [HttpGet]
+    [Route("/GetAddon/{LimitedFeatureId}")]
+    public async Task<IActionResult> GetFeatureAddon(Guid LimitedFeatureId, CancellationToken cancellationToken)
+    {
+        var query = new GetFeatureAddonQuery(LimitedFeatureId);
+        var result = await _sender.Send(query);
+        if (result.IsSuccess)
+            return Ok(result.Value);
+        return NotFound(result.Error);
+    }
+
+
+
+    [HttpPut]
+    [Route("/Recharge/LimitFeature")]
+    public async Task<IActionResult> RechargeLimitedFeature(
+        CreateAddonRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new PurchaseAddonCommand(request.SubscriptionId, request.LimitedFeatureId, request.LimitedFeatureAddonId);
+        var result = await _sender.Send(command, cancellationToken);
+        if (result.IsFailure)
+            return BadRequest(result.Error);
+        return Ok(result);
+    }
+}
---
+++ Viora/Viora.Api/Controllers/Subscriptions/SubscriptionController.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Viora.Application.Subscriptions.ChangeSubscription;
+using Viora.Application.Subscriptions.CreateSubscriptions;
+using Viora.Application.Subscriptions.RenewSubscriptions;
+
+namespace Viora.Api.Controllers.Subscriptions;
+
+[ApiController]
+[Route("api/subscription")]
+public class SubscriptionController : ControllerBase
+{
+    private readonly ISender _sender;
+    public SubscriptionController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost]
+    [Route("/CreateSubscription")]
+    public async Task<IActionResult> CreateSubscriptions(
+        CreateSubscriptionRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new CreateSubscriptionCommand(request.PlanId, request.OrganizationID);
+        var result = await _sender.Send(command, cancellationToken);
+        if (result.IsFailure)
+            return BadRequest(result.Error);
+        return Ok(result.Value);
+    }
+
+    [HttpPut]
+    [Route("/RenewSubscription")]
+    public async Task<IActionResult> RenewSubscription(
+        RenewSubscriptionRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new RenewSubscriptionCommand(request.SubscriptionId);
+        var result = await _sender.Send(command, cancellationToken);
+        if (result.IsFailure)
+            return BadRequest(result.Error);
+        return Ok(result);
+    }
+
+    [HttpPut]
+    [Route("/ChangeSubscriptionPlan")]
+    public async Task<IActionResult> ChangeSubscriptionPlan(
+        ChangeSubscriptionPlanRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new ChangeSubscriptionCommand(request.SubscriptionId, request.NewPlanId);
+        var result = await _sender.Send(command, cancellationToken);
+        if (result.IsFailure)
+            return BadRequest(result.Error);
+        return Ok(result);
+    }
+}
