0238e23ee86f73f91dfec90ed5f3badef61d93df
Mohamed Ahmed <mohamedahmedfathy28@gmail.com>
2026-05-15 16:26:56 +0300

    feat(domain): add initial Schedule entity to domain layer
    - Introduced core entity: Schedule
    - Established relationships with related aggregates (e.g., Tasks, Resources)
    - Prepared domain layer for EF Core configuration and persistence
    - Laid groundwork for commands, queries, and event-driven handlers
    - Provided foundation for enforcing scheduling rules and real-time updates

---
+++ Viora/Viora.Domain/RealTimeScheduling/RecurringScheduleTimeInterval.cs
@@ -0,0 +1,28 @@
+﻿using Viora.Domain.Abstractions;
+
+namespace Viora.Domain.RealTimeScheduling;
+
+public class RecurringScheduleTimeInterval : Entity
+{
+    public Guid RecurringScheduleId { get; private set; }
+    public Guid TimeInterval { get; private set; }
+    public DayOfWeek DayOfWeek { get; private set; }
+    public Guid StaffId { get; private set; }
+    public virtual RecurringSchedule RecurringSchedule { get; private set; }
+    public virtual TimeInterval TimeIntervalEntity { get; private set; }
+
+    public RecurringScheduleTimeInterval()
+    {
+        // For EF Core
+    }
+
+    private RecurringScheduleTimeInterval(Guid id, Guid recurringScheduleId, Guid timeInterval, DayOfWeek dayOfWeek, Guid staffId) : base(id)
+    {
+        RecurringScheduleId = recurringScheduleId;
+        TimeInterval = timeInterval;
+        DayOfWeek = dayOfWeek;
+        StaffId = staffId;
+    }
+
+
+}
---
+++ Viora/Viora.Domain/RealTimeScheduling/ScheduleCancellations.cs
@@ -0,0 +1,29 @@
+﻿using Viora.Domain.Abstractions;
+using Viora.Domain.Users.Customers;
+
+namespace Viora.Domain.RealTimeScheduling;
+
+public class ScheduleCancellations : Entity
+{
+    public Guid CustomerId { get; private set; }
+    public Guid AppointmentId { get; private set; }
+    public DateTime ReservationDate { get; private set; }
+    public DateTime CancellationDate { get; private set; }
+    public string Reason { get; private set; }
+    public virtual Customer Customer { get; private set; }
+    //public virtual Appointment Appointment { get; private set; }
+
+    public ScheduleCancellations()
+    {
+        // For EF Core
+    }
+    private ScheduleCancellations(Guid id, Guid customerId, Guid appointmentId, DateTime reservationDate, DateTime cancellationDate, string reason) : base(id)
+    {
+        CustomerId = customerId;
+        AppointmentId = appointmentId;
+        ReservationDate = reservationDate;
+        CancellationDate = cancellationDate;
+        Reason = reason;
+    }
+
+}
