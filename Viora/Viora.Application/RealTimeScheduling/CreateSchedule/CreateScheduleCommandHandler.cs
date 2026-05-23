47aefea9661868862ce23d8c475d0327d85f68ca
Mohamed Ahmed <mohamedahmedfathy28@gmail.com>
2026-05-23 15:05:42 +0300

    feat(domain): add CreateSchedule command
    - Implemented CreateSchedule command to encapsulate scheduling logic
    - Added validation rules for time ranges and resource availability
    - Prepared command handlers for persistence and event publishing
    - Established foundation for real-time scheduling workflows

---
+++ Viora/Viora.Application/RealTimeScheduling/CreateSchedule/CreateScheduleCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Viora.Application.Abstractions.Messaging;
+using Viora.Domain.Abstractions;
+using Viora.Domain.RealTimeScheduling;
+
+namespace Viora.Application.RealTimeScheduling.CreateSchedule;
+
+public class CreateScheduleCommandHandler(
+    //IBranchRepository branchRepository
+    IScheduleRepository scheduleRepository,
+    IUnitOfWork unitOfWork
+    ) : ICommandHandler<CreateScheduleCommand, Guid>
+{
+    public async Task<Result<Guid>> Handle(CreateScheduleCommand request, CancellationToken cancellationToken)
+    {/*
+        var branch = await branchRepository.GetByIdAsync(request.BranchId, cancellationToken)
+            ?? throw new NotFoundException($"Branch with id {request.BranchId} not found");*/
+        var day = Enum.Parse<DayOfWeek>(request.DayOfWeek);
+
+        var branchSchedule = await scheduleRepository.getByBranchIdAndDayAsync(request.BranchId, day, cancellationToken);
+
+        if (branchSchedule is not null)
+            return Result.Failure<Guid>(ScheduleError.ScheduleOverLap);
+
+        var newSchedule = Schedule.Create(request.BranchId, day);
+        scheduleRepository.Add(newSchedule);
+        await unitOfWork.SaveChangesAsync();
+
+        return Result.Success(newSchedule.Id);
+    }
+}
