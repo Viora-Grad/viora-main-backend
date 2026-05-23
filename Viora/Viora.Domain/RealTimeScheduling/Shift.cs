10da1173a11244632522d0013537f163daa79134
Mohamed Ahmed <mohamedahmedfathy28@gmail.com>
2026-05-23 15:01:03 +0300

    refactor(domain): redesign domain layer architecture
    - Reorganized core entities and aggregates for improved clarity
    - Enhanced consistency across entity naming and property definitions
    - Prepared domain layer for cleaner EF Core configuration and persistence
    - Established foundation for scalable commands, queries, and event-driven workflows
    - Improved maintainability and modularity to support future extensions

---
+++ Viora/Viora.Domain/RealTimeScheduling/Shift.cs
@@ -0,0 +1,36 @@
+﻿using Viora.Domain.Abstractions;
+
+namespace Viora.Domain.RealTimeScheduling;
+
+public class Shift : Entity
+{
+    public Guid ScheduleId { get; private set; }
+    public Guid StaffId { get; private set; }
+    public TimeSpan StartTime { get; private set; }
+    public TimeSpan EndTime { get; private set; }
+
+    //private readonly List<Appointment> _Appointments;
+    //public IReadOnlyCollection<Appointment> Appointments=_Appointment.AsReadOnly();
+
+    public Shift()
+    {
+        // For EF Core
+    }
+
+    private Shift(Guid id, TimeSpan startTime, TimeSpan endTime, Guid scheduleId, Guid staffId) : base(id)
+    {
+        ScheduleId = scheduleId;
+        StartTime = startTime;
+        EndTime = endTime;
+        StaffId = staffId;
+    }
+
+
+    public static Shift Create(Guid scheduleId, TimeSpan startTime, TimeSpan endTime, Guid staffId)
+    {
+        var id = Guid.NewGuid();
+        var newShift = new Shift(id, startTime, endTime, scheduleId, staffId);
+        return newShift;
+    }
+
+}
