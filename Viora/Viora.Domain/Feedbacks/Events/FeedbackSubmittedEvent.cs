9ee64aefb194b1634af4d00912d4dc477f67503a
Ebrahim Mohsen <ebrahim.mohamed@blueoceancorporation.com>
2026-05-30 01:10:16 +0300

    Add feedback module

---
+++ Viora/Viora.Domain/Feedbacks/Events/FeedbackSubmittedEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Viora.Domain.Feedbacks.Events;
+
+/// <summary>
+/// Used to update the overall organization rating related to the branch
+/// </summary>
+/// <param name="BranchId"></param>
+public record FeedbackSubmittedEvent(Guid BranchId);
