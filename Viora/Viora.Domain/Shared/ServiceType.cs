d07febcfdf6eb06c2335e72c8a8b3f9756093f6e
Ebrahim Mohsen <ibrahimjarb69@gmail.com>
2026-06-06 23:54:42 +0300

    Housekeeping changed the order of the services to return them ordered in the reflected method

--- Viora/Viora.Domain/Shared/ServiceType.cs
+++ Viora/Viora.Domain/Shared/ServiceType.cs
@@ -6,30 +6,33 @@ public sealed record ServiceType
 
     private ServiceType(string value) => Value = value;
 
-    public static readonly ServiceType GeneralSurgery = new("General Surgery");
-    public static readonly ServiceType PsychiatryAndBehavioralHealth = new("Psychiatry & Behavioral Health");
-    public static readonly ServiceType Endocrinology = new("Endocrinology");
-    public static readonly ServiceType Pediatrics = new("Pediatrics");
-    public static readonly ServiceType ObstetricsAndGynecology = new("Obstetrics & Gynecology (OB-GYN)");
-    public static readonly ServiceType GastroenterologyAndHepatology = new("Gastroenterology & Hepatology");
+    public static implicit operator ServiceType(string value) => new(value);
+    public static implicit operator string(ServiceType service) => service.Value;
+
     public static readonly ServiceType Cardiology = new("Cardiology");
-    public static readonly ServiceType InternalMedicine = new("Internal Medicine");
-    public static readonly ServiceType ClinicalPharmacology = new("Clinical Pharmacology");
-    public static readonly ServiceType Dermatology = new("Dermatology");
-    public static readonly ServiceType OrthopedicSurgery = new("Orthopedic Surgery");
-    public static readonly ServiceType Hematology = new("Hematology");
-    public static readonly ServiceType Ophthalmology = new("Ophthalmology");
     public static readonly ServiceType ClinicalNutritionAndDietetics = new("Clinical Nutrition & Dietetics");
-    public static readonly ServiceType SexualMedicine = new("Sexual Medicine");
+    public static readonly ServiceType ClinicalPharmacology = new("Clinical Pharmacology");
     public static readonly ServiceType DentistryAndOralHealth = new("Dentistry & Oral Health");
-    public static readonly ServiceType Urology = new("Urology");
-    public static readonly ServiceType Otolaryngology = new("Otolaryngology (ENT)");
-    public static readonly ServiceType Oncology = new("Oncology");
-    public static readonly ServiceType PublicHealthAndPreventiveMedicine = new("Public Health & Preventive Medicine");
-    public static readonly ServiceType PlasticAndReconstructiveSurgery = new("Plastic & Reconstructive Surgery");
+    public static readonly ServiceType Dermatology = new("Dermatology");
+    public static readonly ServiceType Endocrinology = new("Endocrinology");
+    public static readonly ServiceType GastroenterologyAndHepatology = new("Gastroenterology & Hepatology");
+    public static readonly ServiceType GeneralSurgery = new("General Surgery");
+    public static readonly ServiceType Hematology = new("Hematology");
+    public static readonly ServiceType InternalMedicine = new("Internal Medicine");
     public static readonly ServiceType Neurology = new("Neurology");
+    public static readonly ServiceType ObstetricsAndGynecology = new("Obstetrics & Gynecology (OB-GYN)");
+    public static readonly ServiceType Oncology = new("Oncology");
+    public static readonly ServiceType Ophthalmology = new("Ophthalmology");
+    public static readonly ServiceType OrthopedicSurgery = new("Orthopedic Surgery");
+    public static readonly ServiceType Otolaryngology = new("Otolaryngology (ENT)");
+    public static readonly ServiceType Pediatrics = new("Pediatrics");
+    public static readonly ServiceType PlasticAndReconstructiveSurgery = new("Plastic & Reconstructive Surgery");
+    public static readonly ServiceType PsychiatryAndBehavioralHealth = new("Psychiatry & Behavioral Health");
+    public static readonly ServiceType PublicHealthAndPreventiveMedicine = new("Public Health & Preventive Medicine");
     public static readonly ServiceType Pulmonology = new("Pulmonology");
+    public static readonly ServiceType SexualMedicine = new("Sexual Medicine");
     public static readonly ServiceType SportsMedicine = new("Sports Medicine");
+    public static readonly ServiceType Urology = new("Urology");
 
     public static ServiceType FromValue(string value) =>
         All.FirstOrDefault(s => s.Value.Equals(value, StringComparison.OrdinalIgnoreCase))
