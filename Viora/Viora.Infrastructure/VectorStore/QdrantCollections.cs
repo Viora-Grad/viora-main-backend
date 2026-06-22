d6a8529c3c1916cad45376f4ddb8c4eccda3ef43
Mazen Mohamed Nayef <mazennayef2002@gmail.com>
2026-06-22 18:42:31 +0300

    Changed Config of QdrantCollection and updated .gitignore to handle any Mobile Files for integrations

--- Viora/Viora.Infrastructure/VectorStore/QdrantCollections.cs
+++ Viora/Viora.Infrastructure/VectorStore/QdrantCollections.cs
@@ -6,5 +6,5 @@ public class QdrantCollections
     public const string Specialty = "viora_medical_specialties";
     public const string KnowledgeBase = "viora_knowledge";
 
-    public const ulong VectorDimension = 768;
+    public const ulong VectorDimension = 384;
 }
