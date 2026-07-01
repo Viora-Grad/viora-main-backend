7656bd0e423c53305a9e1001a0de8ba5e7ab2c3f
Ebrahim Mohsen <ibrahimjarb69@gmail.com>
2026-07-01 11:59:18 +0300

    Add get my organization by active user id

---
+++ Viora/Viora.Application/Organizations/GetMyOrganization/GetMyOrganizationDetailsQuery.cs
@@ -0,0 +1,6 @@
+﻿using Viora.Application.Abstractions.Messaging;
+using Viora.Application.Organizations.Shared;
+
+namespace Viora.Application.Organizations.GetMyOrganization;
+
+public record GetMyOrganizationDetailsQuery(Guid OwnerId) : IQuery<OrganizationDetailsResponse>;
